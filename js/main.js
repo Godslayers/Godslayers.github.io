75ddd000ebfbdd0cabbb8eb144d7ec9ef496c8a0
fetaxyu <fetaxyu@gmail.com>
2018-12-01 11:08:41 +0800

    init

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(".view-credits").click(function(){
-	$('.navbar-zero').hide();
+	$('.navbar-zero').toggle(100, function() {
+		if ($('.navbar-zero').css('display') == 'none') {
+			$('.view-credits').text('back to index');
+		} else {
+			$('.view-credits').text('view credits');
+		}
+	});
+
 });
